8c9e8091e450d2b4030e114f5124bdcee7fc1a37
Thulani Tyaphile <tj.tyaphile@gmail.com>
2025-02-18 10:48:58 +0200

    Added Swagger API documentation

--- backend/src/main/java/za/co/wedela/backend/config/SecurityConfig.java
+++ backend/src/main/java/za/co/wedela/backend/config/SecurityConfig.java
@@ -1,5 +1,11 @@
 package za.co.wedela.backend.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -36,7 +42,10 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/api/v1/users/auth/login", "/api/v1/users/auth/register")
+                        .requestMatchers("/api/v1/users/auth/login", "/api/v1/users/auth/register",
+                                "/swagger-ui/**", "/v3/api-docs/**", "/v2/api-docs", "/configuration/ui",
+                                "/swagger-resources/**", "/configuration/security", "/swagger-ui.html",
+                                "/webjars/**")
                         .permitAll()
                         .anyRequest().authenticated()
                 )
@@ -45,5 +54,28 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", securityScheme()))
+                .info(new Info()
+                        .title("SQE/QA API Documentation")
+                        .version("1.0")
+                        .description("SQE/QA API Documentation made by SQE team")
+                        .contact(new Contact()
+                                .name("SQE Team")
+                                .url("https://github.com/alpha-thulz/SQE-QA_25")
+                        )
+                );
+    }
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
     }
 }
